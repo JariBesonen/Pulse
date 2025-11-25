d498d926940f995c186b23030c0487d7d847271d
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-11-25 16:22:07 -0700

    Setup Profilepage/add nav profile link functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,26 +5,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar.jsx";
 import DropDownMenu from "./Components/DropDownMenu/DropDownMenu.jsx";
 import Home from "./Pages/Home/Home.jsx";
+import Profile from "./Pages/Profile/Profile.jsx";
 
 function App() {
-  
   const [menuDropdownVisible, setMenuDropdownVisible] = useState(false);
 
   function toggleDropdownMenu() {
-    setMenuDropdownVisible(prev => !prev);
+    setMenuDropdownVisible((prev) => !prev);
   }
-  
+
   return (
     <Router>
-      
       <Navbar toggleDropdownMenu={toggleDropdownMenu} />
-      
-      {menuDropdownVisible && (
-        <DropDownMenu />
-      )}
+
+      {menuDropdownVisible && <DropDownMenu />}
 
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </Router>
   );
---
+++ client/src/Pages/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Profile.css';
+
+function Profile() {
+  return (
+    <div className='profilepage'>
+      <h1 className='profilepage-h1'>Profilepage</h1>
+    </div>
+  )
+}
+
+export default Profile;
