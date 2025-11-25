d72989fabeea3b1ee81590755b3accdf0b71d6eb
Stefan1400 <stefan.besonen783@gmail.com>
2025-11-25 15:48:44 -0700

    Set up hompage

---
+++ client/src/Pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className='homepage'>
+      <h1 className='homepage-h1'>Homepage</h1>
+    </div>
+  )
+}
+
+export default Home
