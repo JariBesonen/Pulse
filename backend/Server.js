624de92ff986307535b0ff3207401e9a3bc70414
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-12-01 15:05:41 -0700

    Feature: setup backend fs and server boilerplate

---
+++ backend/Server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require('helmet');
+const PORT = 3000;
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+app.get("/health", (req, res) => {
+  res.send("server is listening..");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on http://localhost:${PORT}`);
+});
