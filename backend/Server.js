88e3d71be5a1fb00c2ef95bad10809eda58a274f
JariBesonen <149715363+JariBesonen@users.noreply.github.com>
2025-12-02 06:14:50 -0700

    Switched server PORT from 3000 to 5000

--- backend/Server.js
+++ backend/Server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const helmet = require('helmet');
-const PORT = 3000;
+const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
