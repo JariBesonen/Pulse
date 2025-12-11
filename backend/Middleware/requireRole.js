bb9ff271df78209a5e3a212d308f572267aa21b7
Stefan1400 <stefan.besonen783@gmail.com>
2025-12-11 11:01:32 -0700

    Add requireRole.js

---
+++ backend/Middleware/requireRole.js
@@ -0,0 +1,20 @@
+const requireRole = (...allowedRoles) => {
+   return (req, res, next) => {
+      if (!req.user || !req.user.role) {
+         return res.status(401).json({ 
+            success: false, 
+            message: 'user is not authorized '
+         });
+      } 
+
+      if (!allowedRoles.includes(req.user.role)) {
+         return res.status(403).json({ 
+            success: false, 
+            message: 'Forbidden: insufficient role' 
+         });
+      }
+      next();
+   }
+}
+
+module.exports = requireRole;
