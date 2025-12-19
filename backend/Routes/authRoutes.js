89a843a542526df245c328c274dcf0be68208096
Stefan1400 <stefan.besonen783@gmail.com>
2025-12-19 12:15:01 -0700

    feat(auth): Use loginSchema and registerSchema separately in authRoutes.js

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const validateRequest = require('../Middleware/validate.js');
-const { authSchema } = require('../Validation/authSchemas.js');
+const { registerSchema, loginSchema } = require('../Validation/authSchemas.js');
 const { register, login } = require('../Controllers/authController.js');
 
 router.post(
    '/register',
-   validateRequest(authSchema),
+   validateRequest(registerSchema),
    register
 );
 
 router.post(
    '/login',
-   validateRequest(authSchema),
+   validateRequest(loginSchema),
    login
 );
 
