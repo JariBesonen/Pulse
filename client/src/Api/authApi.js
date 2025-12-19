4e5f4b7a675db03146fc06159daff0fc5a9ca856
Stefan1400 <stefan.besonen783@gmail.com>
2025-12-19 12:54:41 -0700

    feat(auth): Add login API in client/authApi.js

--- client/src/Api/authApi.js
+++ client/src/Api/authApi.js
@@ -15,3 +15,19 @@ export const register = async (userData) => {
    return res.json();
 };
 
+export const login = async (userData) => {
+   const res = await fetch('http://localhost:5000/api/users/login', {
+      method: 'POST',
+      headers: {
+         'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(userData)
+   });
+
+   if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error?.error || 'Failed to login. Please try again.');
+   }
+
+   return res.json();
+};
