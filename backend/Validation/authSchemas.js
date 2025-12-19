ca34dc200989ed4a5947eb5b37846c0bf0a96ce5
Stefan1400 <stefan.besonen783@gmail.com>
2025-12-19 11:29:25 -0700

    feat(auth): Add changes to backend/authSchemas.js for better validation

--- backend/Validation/authSchemas.js
+++ backend/Validation/authSchemas.js
@@ -1,10 +1,10 @@
 const { z } = require('zod');
 
 const authSchema = z.object({
-   username: z.string().min(6),
-   email: z.string().min(6),
-   password: z.string().min(6)
-})
+   username: z.string().min(3).max(30),
+   email: z.email().max(254),
+   password: z.string().min(8).max(64)
+});
 
 module.exports = {
    authSchema
