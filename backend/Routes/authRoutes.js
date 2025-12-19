588bac42e26d6ac55aefcdb3bc1adbe5a361e86e
Stefan1400 <stefan.besonen783@gmail.com>
2025-12-19 12:08:55 -0700

    feat(auth): Add login route to authRoutes.js

--- backend/Routes/authRoutes.js
+++ backend/Routes/authRoutes.js
@@ -2,12 +2,18 @@ const express = require('express');
 const router = express.Router();
 const validateRequest = require('../Middleware/validate.js');
 const { authSchema } = require('../Validation/authSchemas.js');
-const { register } = require('../Controllers/authController.js');
+const { register, login } = require('../Controllers/authController.js');
 
 router.post(
    '/register',
    validateRequest(authSchema),
    register
+);
+
+router.post(
+   '/login',
+   validateRequest(authSchema),
+   login
 );
 
 module.exports = router;
